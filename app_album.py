2e2e4f6e1ee131a20d74f6f3d15d3f81f868a9eb
deesii <donnagph@hotmail.com>
2024-02-18 18:02:05 +0800

    updated sequence diagram for the album (music store)

--- app_album.py
+++ app_album.py
@@ -10,7 +10,7 @@ connection.connect()
 connection.seed("seeds/music_library.sql")
 
 # Retrieve all albums
-album_repository = AlbumRepository(connection)
+album_repository = AlbumRepository(connection) # creates an instance of the album repository, whilst opening and ensuring the connection of the database so that there can a place to store that connection and output of the queries
 albums = album_repository.all()
 
 # List them out
--- lib/artist_repository.py
+++ lib/artist_repository.py
@@ -18,15 +18,14 @@ class ArtistRepository:
     # Find a single artist by their id
     def find(self, artist_id):
         rows = self._connection.execute(
-            'SELECT * from artists WHERE id = %s', [artist_id])
+            'SELECT * from artists WHERE id = %s', [artist_id]) #
         row = rows[0]
-        return Artist(row["id"], row["name"], row["genre"])
+        return Artist(row["id"], row["name"], row["genre"]) # these are instance of objects
 
     # Create a new artist
     # Do you want to get its id back? Look into RETURNING id;
     def create(self, artist):
-        self._connection.execute('INSERT INTO artists (name, genre) VALUES (%s, %s)', [
-                                 artist.name, artist.genre])
+        self._connection.execute('INSERT INTO artists (name, genre) VALUES (%s, %s)', [artist.name, artist.genre])
         return None
 
     # Delete an artist by their id
